0e9a0f2dcdcf9a3b01714777c66be4a87c493b81
satya prakash <satya11196@gmail.com>
2020-10-19 21:44:03 +0530

    basic form validation working

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-
+Vue.use(vuelidate.default)
 const database = require('./js/database');
 var update = true;
 
@@ -10,157 +10,203 @@ window.onload = function() {
       product: 'Welcome to Employee Portal',
     },
   })
-  console.log(update);
-  var upd = new Vue({
-    el: '#btn',
-    data:{
-      update: update
+  var form = new Vue({
+    el:'#cont',
+    data(){
+      return{
+        form: {
+          name: null,
+          age: null,
+        }
+      }
+    },
+    validations:{
+      form:{
+        name:{
+          required: validators.required
+        },
+        age:{
+          required: validators.required,
+          integer: validators.integer,
+          min: validators.minValue(1)
+        }
+      }
+    },
+    // computed: {
+    //   nameIsValid(){
+    //     return !!this.form.name
+    //   },
+    //   ageIsValid(){
+    //     return typeof this.form.age === 'number'
+    //   },
+    //   formIsValid(){
+    //     return this.nameIsValid && this.ageIsValid
+    //   } 
+    // }, 
+    methods: {
+      submitForm(){
+        // const formIsValid = this.nameIsValid && this.ageIsValid
+        if(!this.$v.form.$invalid){
+          console.log("form submitted",this.form)
+        }
+        else{
+          console.log("invalid form")
+        }
+      }
     }
   })
+   
+  console.log(update);
+  // var upd = new Vue({
+  //   el: '#btn',
+  //   data:{
+  //     update: update
+  //   }
+  // })
   // Populate the table
   populateTable();
 
   // Add the add button click event
-  document.getElementById('add').addEventListener('click', () => {
-    console.log("Entered add click evetn");
-    // Retrieve the input fields
-    var emp_id = document.getElementById('emp_id');
-    var firstname = document.getElementById('firstname');
-    var lastname = document.getElementById('lastname');
-    var email = document.getElementById('email');
-    var gender = document.getElementById('gender');
-    var dob = document.getElementById('dob');
-    var mobile = document.getElementById('mobile');
-    var address1 = document.getElementById('address1');
-    var address2 = document.getElementById('address2');
-    var pincode = document.getElementById('pincode');
-    var countrycode = document.getElementById('countrycode');
+  // document.getElementById('add').addEventListener('click', () => {
+  //   console.log("Entered add click evetn");
+  //   // Retrieve the input fields
+  //   var emp_id = document.getElementById('emp_id');
+  //   var firstname = document.getElementById('firstname');
+  //   var lastname = document.getElementById('lastname');
+  //   var email = document.getElementById('email');
+  //   var gender = document.getElementById('gender');
+  //   var dob = document.getElementById('dob');
+  //   var mobile = document.getElementById('mobile');
+  //   var address1 = document.getElementById('address1');
+  //   var address2 = document.getElementById('address2');
+  //   var pincode = document.getElementById('pincode');
+  //   var countrycode = document.getElementById('countrycode');
 
-    // Save the person in the database
-    database.addPerson(emp_id.value,
-      firstname.value, 
-      lastname.value,
-      email.value,
-      gender.value,
-      dob.value,
-      mobile.value,
-      address1.value,
-      address2.value,
-      pincode.value,
-      countrycode.value
-      );
+  //   // Save the person in the database
+  //   database.addPerson(emp_id.value,
+  //     firstname.value, 
+  //     lastname.value,
+  //     email.value,
+  //     gender.value,
+  //     dob.value,
+  //     mobile.value,
+  //     address1.value,
+  //     address2.value,
+  //     pincode.value,
+  //     countrycode.value
+  //     );
 
-    // Reset the input fields
-    emp_id.value=''
-    firstname.value = '';
-    lastname.value = '';
-    email.value='';
-    gender.value = '';
-    dob.value = '';
-    mobile.value = '';
-    address1.value = '';
-    address2.value = '';
-    pincode.value 
-    countrycode.value = '';
+  //   // Reset the input fields
+  //   emp_id.value=''
+  //   firstname.value = '';
+  //   lastname.value = '';
+  //   email.value='';
+  //   gender.value = '';
+  //   dob.value = '';
+  //   mobile.value = '';
+  //   address1.value = '';
+  //   address2.value = '';
+  //   pincode.value 
+  //   countrycode.value = '';
 
-    // Repopulate the table
-    populateTable();
-  });
+  //   // Repopulate the table
+  //   populateTable();
+  // });
 
-  document.getElementById('update').addEventListener('click', () => {
+  // document.getElementById('update').addEventListener('click', () => {
     
-    console.log("Entered update click event");
-    // Retrieve the updated input fields
-    var emp_id = document.getElementById('emp_id');
-    var firstname = document.getElementById('firstname');
-    var lastname = document.getElementById('lastname');
-    var email = document.getElementById('email');
-    var gender = document.getElementById('gender');
-    var dob = document.getElementById('dob');
-    var mobile = document.getElementById('mobile');
-    var address1 = document.getElementById('address1');
-    var address2 = document.getElementById('address2');
-    var pincode = document.getElementById('pincode');
-    var countrycode = document.getElementById('countrycode');
-    console.log (typeof(emp_id.value));
+  //   console.log("Entered update click event");
+  //   // Retrieve the updated input fields
+  //   var emp_id = document.getElementById('emp_id');
+  //   var firstname = document.getElementById('firstname');
+  //   var lastname = document.getElementById('lastname');
+  //   var email = document.getElementById('email');
+  //   var gender = document.getElementById('gender');
+  //   var dob = document.getElementById('dob');
+  //   var mobile = document.getElementById('mobile');
+  //   var address1 = document.getElementById('address1');
+  //   var address2 = document.getElementById('address2');
+  //   var pincode = document.getElementById('pincode');
+  //   var countrycode = document.getElementById('countrycode');
+  //   console.log (typeof(emp_id.value));
 
-    db.update({ _id: parseInt(emp_id.value) }, { $set: 
-      { 
-        firstname: firstname.value,
-        lastname: lastname.value,
-        email: email.value,
-        gender: gender.value,
-        dob: dob.value,
-        mobile: mobile.value,
-        address1: address1.value,
-        address2: address2.value,
-        pincode: pincode.value,
-        countrycode: countrycode.value
-      } }, { multi: true }, function (err, numReplaced) {
-      console.log(numReplaced);
-    });
+  //   db.update({ _id: parseInt(emp_id.value) }, { $set: 
+  //     { 
+  //       firstname: firstname.value,
+  //       lastname: lastname.value,
+  //       email: email.value,
+  //       gender: gender.value,
+  //       dob: dob.value,
+  //       mobile: mobile.value,
+  //       address1: address1.value,
+  //       address2: address2.value,
+  //       pincode: pincode.value,
+  //       countrycode: countrycode.value
+  //     } }, { multi: true }, function (err, numReplaced) {
+  //     console.log(numReplaced);
+  //   });
 
-    // database.deletePerson(emp_id.value);
-    populateTable();
+  //   // database.deletePerson(emp_id.value);
+  //   populateTable();
 
-    // database.addPerson(emp_id.value,
-    //   firstname.value, 
-    //   lastname.value,
-    //   email.value,
-    //   gender.value,
-    //   dob.value,
-    //   mobile.value,
-    //   address1.value,
-    //   address2.value,
-    //   pincode.value,
-    //   countrycode.value
-    //   );
-    // populateTable();
+  //   // database.addPerson(emp_id.value,
+  //   //   firstname.value, 
+  //   //   lastname.value,
+  //   //   email.value,
+  //   //   gender.value,
+  //   //   dob.value,
+  //   //   mobile.value,
+  //   //   address1.value,
+  //   //   address2.value,
+  //   //   pincode.value,
+  //   //   countrycode.value
+  //   //   );
+  //   // populateTable();
 
-    // Reset the input fields
-    emp_id.value=''
-    firstname.value = '';
-    lastname.value = '';
-    email.value='';
-    gender.value = '';
-    dob.value = '';
-    mobile.value = '';
-    address1.value = '';
-    address2.value = '';
-    pincode.value = '';
-    countrycode.value = '';
+  //   // Reset the input fields
+  //   emp_id.value=''
+  //   firstname.value = '';
+  //   lastname.value = '';
+  //   email.value='';
+  //   gender.value = '';
+  //   dob.value = '';
+  //   mobile.value = '';
+  //   address1.value = '';
+  //   address2.value = '';
+  //   pincode.value = '';
+  //   countrycode.value = '';
 
 
-    // db.find({_id:parseInt(emp_id)}, function(err,docs){
-      // if(err){
-      //   console.log("Employee must be added first");
-      // }
-      // else{
-      // db.update({_id:parseInt(emp_id)},{lastname:'Bhasin'},{},function(err,numReplaced){
-      //   console.log(numReplaced);
-      // });
-      // }
-      //   db.update({_id:parseInt(emp_id)}, {
-      //     firstname:firstname, 
-      //     lastname:lastname,
-      //     email: email,
-      //     gender: gender,
-      //     dob: dob,
-      //     mobible: mobile,
-      //     address1: address1,
-      //     address2: address2,
-      //     pincode:pincode,
-      //     countrycode:countrycode
-      //   }, {}, function (err, numReplaced) {
-      //     if(err)
-      //     {
-      //       console.log(err);
-      //     }
-      //   console.log("Employee with id: "+ numReplaced+" is updated")  
-      //   });
-      // }
-    // });
-  });
+  //   // db.find({_id:parseInt(emp_id)}, function(err,docs){
+  //     // if(err){
+  //     //   console.log("Employee must be added first");
+  //     // }
+  //     // else{
+  //     // db.update({_id:parseInt(emp_id)},{lastname:'Bhasin'},{},function(err,numReplaced){
+  //     //   console.log(numReplaced);
+  //     // });
+  //     // }
+  //     //   db.update({_id:parseInt(emp_id)}, {
+  //     //     firstname:firstname, 
+  //     //     lastname:lastname,
+  //     //     email: email,
+  //     //     gender: gender,
+  //     //     dob: dob,
+  //     //     mobible: mobile,
+  //     //     address1: address1,
+  //     //     address2: address2,
+  //     //     pincode:pincode,
+  //     //     countrycode:countrycode
+  //     //   }, {}, function (err, numReplaced) {
+  //     //     if(err)
+  //     //     {
+  //     //       console.log(err);
+  //     //     }
+  //     //   console.log("Employee with id: "+ numReplaced+" is updated")  
+  //     //   });
+  //     // }
+  //   // });
+  // });
 
   populateTable();
 }  
